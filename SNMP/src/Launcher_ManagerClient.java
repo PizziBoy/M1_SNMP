3c9f300c0936204fa7fc273d644a9f21bb14a51c
Guilhaume de Gineste <guilhaume.degineste@gmail.com>
2023-02-01 18:24:25 +0100

    merge agent + manager with BUGS RMI

---
+++ SNMP/src/Launcher_ManagerClient.java
@@ -0,0 +1,62 @@
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Launcher_ManagerClient {
+
+	public static void main(String [] args) throws MalformedURLException, RemoteException, NotBoundException {
+		Scanner scan = new Scanner(System.in);
+		boolean menuOn = true;
+		
+		Agent agent =  (Agent) Naming.lookup("rmi://localhost:20999/agent");
+		
+		while(menuOn) {
+			//Affichage du menu
+			System.out.println("1 - GET");
+			System.out.println("2 - SET");
+			System.out.println("O - LEAVE");
+			
+			//Parametres requetes
+			ParameterGet paramG = new ParameterGet(null);
+			ParameterSet paramS = new ParameterSet(null, null);
+			
+			try {
+				int valMenu = scan.nextInt();
+				scan.nextLine();
+				switch(valMenu) {
+				case 0:
+					menuOn = false;
+					System.out.println("You choose to leave!");
+					break;
+				case 1: 
+					System.out.println("You choose GET!");
+					System.out.println("Please enter the key : ");
+					paramG.setName(scan.next());
+					System.out.println(paramG.getName());
+					System.out.println(agent.get(paramG).toString());
+					break;
+				case 2:
+					System.out.println("You choose SET!");
+					System.out.println("Please enter the key : ");
+					paramS.setName(scan.next());
+					scan.nextLine();
+					
+					System.out.println("Please enter the new value : ");
+					paramS.setValue(scan.next());
+					break;
+				}
+				
+			} catch (InputMismatchException excep) {
+				System.err.println("ERROR : Please enter a number between 0 and 2");
+			}
+			
+		}
+		
+		scan.close();
+	}
+
+}
