b2c6a8a8c95039898e0119c0fe9d7e41ce48ee68
Guilhaume de Gineste <guilhaume.degineste@gmail.com>
2023-02-08 17:54:08 +0100

    starting javadoc

--- SNMP/src/Agent.java
+++ SNMP/src/Agent.java
@@ -2,7 +2,9 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.HashMap;
 
-
+/**
+ * Agent interfaces for RMI implements
+ */
 public interface Agent extends Remote{
 	Message get(ParameterGet parameterGet) throws RemoteException;
 	Message set(ParameterSet parameterSet) throws RemoteException;
--- SNMP/src/Manager.java
+++ SNMP/src/Manager.java
@@ -1,21 +1,39 @@
 import java.rmi.RemoteException;
 
+/**
+ * 
+ * This class allow user to instanciate manager object
+ *
+ */
 public class Manager {
 
 	private Agent agent;
 	
+	/**
+	 * Manager constructor
+	 */
 	public Manager() {
 		super();
 	}
 	
+	/**
+	 * Method for allow Manager to manage @param agent 
+	 */
 	public void bindAgent(Agent agent) {
 		this.agent = agent;
 	}
-	
+	/**
+	 * Get variable value by requesting managed agent with parameter @param pGet
+	 * @throws RemoteException
+	 */
 	public Message getFromAgent(ParameterGet pGet) throws RemoteException {
 		return this.agent.get(pGet);
 	}
 	
+	/**
+	 * Set variable value by requesting managed agent with parameter @param pSet
+	 * @throws RemoteException
+	 */
 	public Message setOnAgent(ParameterSet pSet) throws RemoteException {
 		return this.agent.set(pSet);
 	}
