60fd2978532843285bba6215b05f824ca6095871
Elie Jourdan <elie31620@hotmail.fr>
2023-02-16 22:43:53 +0100

    Merge branch 'main' of github.com:PizziBoy/M1_SNMP
    putValue in the MIB work

--- SNMP/src/AgentImpl.java
+++ SNMP/src/AgentImpl.java
@@ -1,5 +1,7 @@
 import java.io.Serializable;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
@@ -21,10 +23,13 @@ public class AgentImpl extends UnicastRemoteObject implements Agent {
 	 * @param communityConfig is the hashMap containing <community, Permissions>
 	 * @throws RemoteException
 	 * Create a new MIB object 
+	 * @throws UnknownHostException 
+	 * @throws SocketException 
 	 */
-	public AgentImpl(HashMap<String, Droit> communityConfig) throws RemoteException {
+	public AgentImpl(HashMap<String, Droit> communityConfig) throws RemoteException, SocketException, UnknownHostException {
 		this.mib = new MIB();
 		this.communityConfig = communityConfig;
+		this.putMibValue();
 	}
 	
 
@@ -125,11 +130,97 @@ public class AgentImpl extends UnicastRemoteObject implements Agent {
 		return this.communityConfig.get(community);
 	}
 	
+	/**
+	 * get the PC operating system
+	 * @return the OS
+	 */
+	public String getOs() {
+		String os = System.getProperty("os.name").toLowerCase();
+		return os;
+	}
 	
+	/**
+	 * get the localhost with his name and his ip
+	 * @return the host
+	 * @throws UnknownHostException
+	 */
+	public InetAddress getLocalHost() throws UnknownHostException {
+		InetAddress hostInterface = InetAddress.getLocalHost();
+		return hostInterface;
+	}
+	
+	/**
+	 * get the inet host interface 
+	 * @param hostInterface
+	 * @return this inetInterface
+	 * @throws SocketException
+	 */
+	public NetworkInterface getInetInterface(InetAddress hostInterface) throws SocketException {
+		 NetworkInterface inetInterface = NetworkInterface.getByInetAddress(hostInterface);
+		 return inetInterface;
+	}
+	
+	/**
+	 * get the interface status (true = up and false = down) 
+	 * @param inetInterface
+	 * @return the interface status
+	 * @throws SocketException
+	 */
+	public String statusInterfaceUp(NetworkInterface inetInterface) throws SocketException {
+		 boolean interfaceUp = inetInterface.isUp();
+		 return String.valueOf(interfaceUp);
+	}
+
+	/**
+	 * get the host ip address
+	 * @param hostInterface
+	 * @return the ip address of that host
+	 * @throws UnknownHostException
+	 */
+	public String getIpAdress(InetAddress hostInterface) throws UnknownHostException {
+		String ipAddress = hostInterface.getHostAddress();
+		return ipAddress;
+	}
+	
+	/**
+	 * get the interface mac address
+	 * @param inetInterface
+	 * @return te mac address of that interface
+	 * @throws SocketException
+	 */
+	public String getMacAdress(NetworkInterface inetInterface) throws SocketException {
+		 byte[] hardwareAddress = inetInterface.getHardwareAddress();
+		 String[] hexadecimal = new String[hardwareAddress.length];
+		 for (int i = 0; i < hardwareAddress.length; i++) {
+			 hexadecimal[i] = String.format("%02X", hardwareAddress[i]);
+		 }
+		 String macAddress = String.join("-", hexadecimal);
+		 return macAddress;
+	}
+	
+	/**
+	 * put the values the agent MIB
+	 * @throws SocketException
+	 * @throws UnknownHostException
+	 */
+	public void putMibValue() throws SocketException, UnknownHostException {
+		String[] objectMib = {"os","addrIp","addrMac","statusInterface"};
 		
-
-
-
-
+		String os = this.getOs();
+		
+		InetAddress host = this.getLocalHost();
+		String ipAddress = this.getIpAdress(host);
+		
+		NetworkInterface inetInterface = this.getInetInterface(host);
+		String macAddress = this.getMacAdress(inetInterface);
+		String interfaceIsUp = this.statusInterfaceUp(inetInterface);
+	  
+	    String[] MibValue = {os,ipAddress,macAddress,interfaceIsUp};
+	    
+	    
+	    for (int i = 0; i < objectMib.length; i++) {
+			mib.setValueMib(objectMib[i], MibValue[i]);
+		}
+	}	
 	
 }
