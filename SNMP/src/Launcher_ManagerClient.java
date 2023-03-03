2052fcd46dd6dbb2a00e8b24f96319bc586e5358
Guilhaume de Gineste <guilhaume.degineste@gmail.com>
2023-03-03 13:12:50 +0100

    V2 OK (fix ctrl operation MIB + getNext)

--- SNMP/src/Launcher_ManagerClient.java
+++ SNMP/src/Launcher_ManagerClient.java
@@ -12,48 +12,71 @@ public class Launcher_ManagerClient {
 		Scanner scan = new Scanner(System.in);
 		boolean menuOn = true;
 		
-		
+		String community = "public";
 		Manager manager = new Manager();
 		manager.bindAgent((Agent) Naming.lookup("rmi://localhost:20999/agent"));
 		
+		System.out.println("Do you have a community");
+		System.out.println("1 - YES");
+		System.out.println("2 - NO");
+		int valMenu = scan.nextInt();
+		switch(valMenu) {
+		case 1:
+			System.out.println("Enter your community : ");
+			community = scan.next();
+			break;
+		case 2:
+			System.out.println("Your default community is public");
+			break;
+		}
 		while(menuOn) {
 			//Affichage du menu
-			System.out.println("1 - GET");
-			System.out.println("2 - SET");
-			System.out.println("O - LEAVE");
+			
+			
+			System.out.println("["+community+"] "+"1 - GET");
+			System.out.println("["+community+"] "+"2 - SET");
+			System.out.println("["+community+"] "+"3 - GET-NEXT");
+			System.out.println("["+community+"] "+"O - LEAVE");
 			
 			//Parametres requetes
-			ParameterGet paramG = new ParameterGet(null);
-			ParameterSet paramS = new ParameterSet(null, null);
-			
+			ParameterGet paramG = new ParameterGet(null,community);
+			ParameterSet paramS = new ParameterSet(null,community,null);
+			paramG.setCommunity(community);
+			paramS.setCommunity(community);
 			try {
-				int valMenu = scan.nextInt();
+				valMenu = scan.nextInt();
 				scan.nextLine();
 				switch(valMenu) {
 				case 0:
 					menuOn = false;
-					System.out.println("You choose to leave!");
+					System.out.println("["+community+"] "+"You choose to leave!");
 					break;
 				case 1: 
-					System.out.println("You choose GET!\n");
-					System.out.println("Please enter the key : \n");
+					System.out.println("["+community+"] "+"You choose GET!\n");
+					System.out.println("["+community+"] "+"Please enter the key : \n");
 					paramG.setName(scan.next());
 					System.out.println(manager.getFromAgent(paramG).getValue());
 					break;
 				case 2:
-					System.out.println("You choose SET!\n");
-					System.out.println("Please enter the key : \n");
+					System.out.println("["+community+"] "+"You choose SET!\n");
+					System.out.println("["+community+"] "+"Please enter the key : \n");
 					paramS.setName(scan.next());
 					scan.nextLine();
 					
-					System.out.println("Please enter the new value : \n");
+					System.out.println("["+community+"] "+"Please enter the new value : \n");
 					paramS.setValue(scan.next());
 					System.out.println(manager.setOnAgent(paramS).getValue());
+					break;
+				case 3: 
+					System.out.println("["+community+"] "+"You choose GET-NEXT!\n");
+					System.out.println("["+community+"] "+"Please enter the key : \n");
+					paramG.setName(scan.next());
+					System.out.println(manager.getNextFromAgent(paramG).getValue());
 					break;
 				}
 				
 			} catch (InputMismatchException excep) {
-				System.err.println("ERROR : Please enter a number between 0 and 2");
+				System.err.println("ERROR : Please enter a number between 0 and 3");
 			}
 			
 		}
